e8f0303a385341379378a67cd94997e14bfd52b4
praks0208 <praks.sawant26@gmail.com>
2024-05-26 20:09:12 +0530

    Sharing State Across Components

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -12,8 +12,12 @@ export default function TimerChallenge({ title, targetTime }) {
     const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
     if (timeRemaining <= 0) {
-        clearInterval(timer.current);setTimeRemaining(targetTime * 1000);
+        clearInterval(timer.current);
         dialog.current.open();
+    }
+
+    function handleReset(){
+        setTimeRemaining(targetTime * 1000);
     }
 
     function handleStart() {
@@ -29,7 +33,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
     return (
         <>
-            <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+            <ResultModal 
+            ref={dialog} 
+            targetTime={targetTime} 
+            remainingTime={timeRemaining} 
+            onReset={handleReset}
+            />
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">
